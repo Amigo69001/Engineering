e65561ce87fe74ad535921a0ac183e098f77ccff
Amir Garibmamadov <amirgaribmamadov5@gmail.com>
2026-04-23 19:40:56 +0300

    mod2

--- Dac4.py
+++ Dac4.py
@@ -1,53 +1,44 @@
 import smbus
-import time
 
-class MCP4725:
-    def __init__(self, dynamic_range, address=0x61, verbose=True):
+class set_U_num():
+    def __init__(self, dynamic_range, adress = 0x61, verbose = True):
         self.bus = smbus.SMBus(1)
-        time.sleep(1)
 
-        self.address = address
-        self.wm = 0x00
+        self.adress = adress
+        self.vm  = 0x00
         self.pds = 0x00
 
         self.verbose = verbose
         self.dynamic_range = dynamic_range
 
+    def set_number(self, number):
+        if (number > 0xFFF):
+            print("num out of range")
+            return 0
+        fb = self.vm | self.pds | number >> 8
+        sb = number & 0xFF
+
+        self.bus.write_byte_data(self.adress, fb, sb)
+
+        if self.verbose:
+            print(f"adress: {self.adress}\n\tfb = 0x{fb:02x}\n\tsb = 0x{sb:02x}\n")
+
+    def put_U(self, U):
+        number = int(0xFFF * U / self.dynamic_range)
+        self.set_number(number)
+
     def deinit(self):
         self.bus.close()
 
-    def set_number(self, number):
-        if not isinstance(number, int):
-            print("На вход ЦАП можно подавать только целые числа")
-
-        if not (0 <= number <= 4095):
-            print("Число выходит на разрядность MCP4725 (12 бит)")
-
-        first_byte = self.wm | self.pds | number >> 8
-        second_byte = number & 0xFF
-        self.bus.write_byte_data(0x61, first_byte, second_byte)
-
-        if self.verbose:
-            print(f"Число: {number}, отправленные по I2C данные:\
-                [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
-        
-    def set_voltage(self, voltage):
-        if (0 <= voltage <= self.dynamic_range):
-            number = int(voltage / self.dynamic_range * 4095)
-            self.set_number(number)
-        else:
-            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
-
-
 if __name__ == "__main__":
-    try:
-        mcp = MCP4725(5.11)
+    dac = set_U_num(5.284)
 
-        while True:
+    try:
+        while 1:
             try:
-                voltage = float(input("Введите напряжение в вольтах: "))
-                mcp = mcp.set_voltage(voltage)
+                U = float(input("Введите напряжение в Вольтах:"))
+                dac.put_U(U)
             except ValueError:
-                print("Вы ввели не число. Попробуйте еще раз")
+                print("это не число")
     finally:
-        mcp.deinit()
+        dac.deinit()
