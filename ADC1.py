d4d187b683d7d7884a782cd0bcb1f7e5441d2574
Amir Garibmamadov <amirgaribmamadov5@gmail.com>
2026-05-07 18:46:46 +0300

    T1

--- ADC1.py
+++ ADC1.py
@@ -1,57 +1,47 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
 
-dynamic_range = 3.304
+class R2R_ADC:
+    def __init__(self, dynamic_range, compare_time = 0.1, verbose = False):
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        self.compare_time = compare_time
 
-
-class R2R_DAC():
-    def __init__(self, dynamic_range, comporator_time, verbose = False):
-        self.gpio_bits = [26,20,19,16,13,12,25,11][::-1]
+        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
         self.comp_gpio = 21
 
-        self.comporator_time = comporator_time
-        self.dynamic_range   = dynamic_range
-        self.verbose         = verbose
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.bits_gpio, GPIO.OUT, initial=0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
 
-        gpio.setmode(gpio.BCM)
+    def deinit(self):
+        GPIO.output(self.bits_gpio, 0)
+        GPIO.cleanup()
 
-        gpio.setup(self.gpio_bits, gpio.OUT, initial = 0)
-        gpio.setup(self.comp_gpio, gpio.IN)
+    def number_to_dac(self, number):
+        a = [int(i) for i in bin(number)[2:].zfill(8)]
+        for i in range(len(a)):
+            GPIO.output(self.bits_gpio[i], a[i])
 
-    def num_to_voltage(self, num):
-        if not (0 < num < 256):
-            return 0
-        return self.dynamic_range * num / 255
-
-    def lights(self, num):
-        for out in self.gpio_bits:
-            gpio.output(out,num % 2)
-            num //= 2
-        return
-    
-    def put_U(self):
-        num = 0
-        self.lights(num)
-
-        while (not gpio.input(self.comp_gpio)) and num < 256:
-            num += 1
-            self.lights(num)
+    def sequential_counting_adc(self):
+        for dac_value in range(256):
+            self.number_to_dac(dac_value)
             time.sleep(0.01)
+            time.sleep(0.0001)
+            comparator_output = GPIO.input(self.comp_gpio)
+            if comparator_output == 1:
+                return dac_value
+        return 255
 
-        print(f"Напряжение: {self.num_to_voltage(num - 1):2f}V")
-    
-    def destroy(self):
-        gpio.output(self.gpio_bits, 0)
-        gpio.cleanup()
+    def get_sc_voltage(self):
+        digital_value = self.sequential_counting_adc()
+        voltage = (digital_value / 255) * self.dynamic_range
+        return voltage
 
-try:
-    check_U = R2R_DAC(dynamic_range, 0.1)
-    while 1:
-        try:
-            check_U.put_U()
-        except ValueError:
-            print("это не число")
-        
-
-finally:
-    check_U.destroy()
+if __name__ == "__main__":
+    try:
+        r2r_adc = R2R_ADC(3.3)
+        while True:
+            voltage = r2r_adc.get_sc_voltage()
+            print(f'Напряжение: {voltage} В')
+    finally:
