0fc8f1f0ccc4383105142ee64e2fd134a177d3e0
Amir Garibmamadov <amirgaribmamadov5@gmail.com>
2026-05-07 17:45:29 +0300

    FIN

--- ADC1.py
+++ ADC1.py
@@ -1,76 +1,57 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
 
-class R2R_ADC:
-    def __init__(self, dynamic_range, compare_range = 0.01, verbose = False):
-        self.dynamic_range = dynamic_range
-        self.verbose = verbose
-        self.compare_time = compare_range
+dynamic_range = 3.304
 
-        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+
+class R2R_DAC():
+    def __init__(self, dynamic_range, comporator_time, verbose = False):
+        self.gpio_bits = [26,20,19,16,13,12,25,11][::-1]
         self.comp_gpio = 21
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
-        GPIO.setup(self.comp_gpio, GPIO.IN)
+        self.comporator_time = comporator_time
+        self.dynamic_range   = dynamic_range
+        self.verbose         = verbose
 
-    def deinit(self):
-        GPIO.output(self.bits_gpio, 0)
-        GPIO.cleanup()
+        gpio.setmode(gpio.BCM)
 
-    def number_to_dac(self, number):
-        bits = [int(element) for element in bin(number)[2:].zfill(8)]
-        GPIO.output(self.bits_gpio, bits)
+        gpio.setup(self.gpio_bits, gpio.OUT, initial = 0)
+        gpio.setup(self.comp_gpio, gpio.IN)
 
-    def sequential_counting_adc(self):
-        for value in range(256):
-            R2R_ADC.number_to_dac(self, value)
-            time.sleep(self.compare_time)
-            if (GPIO.input(self.comp_gpio) ==  1):
-                return value
-        return 255
+    def num_to_voltage(self, num):
+        if not (0 < num < 256):
+            return 0
+        return self.dynamic_range * num / 255
 
-    def get_sc_voltage(self):
-        value = R2R_ADC.sequential_counting_adc(self)
-        return (value/255) * self.dynamic_range
+    def lights(self, num):
+        for out in self.gpio_bits:
+            gpio.output(out,num % 2)
+            num //= 2
+        return
+    
+    def put_U(self):
+        num = 0
+        self.lights(num)
 
-    def successive_approximation_adc(self):
-        right = 256
-        left  = 0
-        value = 0
-        while ((right - left) > 1):
-            value = (right + left) / 2
-            R2R_ADC.number_to_dac(self, value)
-            time.sleep(self.compare_time)
-            if (GPIO.input(self.comp_gpio) == 1):
-                right = value
-            else:
-                left = value
-        return value
+        while (not gpio.input(self.comp_gpio)) and num < 256:
+            num += 1
+            self.lights(num)
+            time.sleep(0.01)
 
-    def get_sar_voltage(self):
-        value = self.successive_approximation_adc(self)
-        return (value/255) * self.dynamic_range
+        print(f"Напряжение: {self.num_to_voltage(num - 1):2f}V")
+    
+    def destroy(self):
+        gpio.output(self.gpio_bits, 0)
+        gpio.cleanup()
 
+try:
+    check_U = R2R_DAC(dynamic_range, 0.1)
+    while 1:
+        try:
+            check_U.put_U()
+        except ValueError:
+            print("это не число")
+        
 
-
-
-
-# if __name__ == "__main__":
-    # try:
-        # adc = R2R_ADC(3.3, 0.01)
-        # while True:
-            # voltage = adc.get_sc_voltage()
-            # print(f"Напряжение: {voltage}")
-    # finally:
-        # adc.deinit()
-
-if __name__ == "__main__":
-    try:
-        adc = R2R_ADC(3.3, 0.01)
-        while True:
-            voltage = adc.get_sar_voltage(adc)
-            print (f"Напряжение: {voltage}")
-
-    finally:
-        adc.deinit()
+finally:
+    check_U.destroy()
